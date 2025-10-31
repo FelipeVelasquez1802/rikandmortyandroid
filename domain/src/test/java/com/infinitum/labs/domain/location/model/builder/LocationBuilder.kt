4f1daf3359e02eed97165acae6e04b1d91caa5fc
Felipe Velásquez <andres.diaz.velasquez@unillanos.edu.co>
2025-10-31 12:03:05 -0500

    refactor(testing): Remove KDocs from test files
    This commit removes KDoc comments from several test and test utility files to clean up boilerplate documentation.
    
    - Removes KDocs from `ExampleUnitTest` and `ExampleInstrumentedTest` in the `presentation` module.
    - Removes KDocs from `LocationBuilder` in the `domain` module's test sources.

--- domain/src/test/java/com/infinitum/labs/domain/location/model/builder/LocationBuilder.kt
+++ domain/src/test/java/com/infinitum/labs/domain/location/model/builder/LocationBuilder.kt
@@ -2,16 +2,6 @@ package com.infinitum.labs.domain.location.model.builder
 
 import com.infinitum.labs.domain.location.model.Location
 
-/**
- * Builder for creating Location test instances.
- * Provides sensible defaults and convenient factory methods for common locations.
- *
- * Usage:
- * ```
- * val location = LocationBuilder.aLocation().withName("Earth").build()
- * val earth = LocationBuilder.earthC137()
- * ```
- */
 class LocationBuilder {
     private var id: Int = 1
     private var name: String = "Earth (C-137)"
@@ -51,14 +41,8 @@ class LocationBuilder {
     }
 
     companion object {
-        /**
-         * Creates a basic location builder with default values.
-         */
         fun aLocation() = LocationBuilder()
 
-        /**
-         * Creates Earth (C-137) - The home dimension of the original Rick and Morty.
-         */
         fun earthC137() = LocationBuilder()
             .withId(1)
             .withName("Earth (C-137)")
@@ -73,9 +57,6 @@ class LocationBuilder {
             .withUrl("https://rickandmortyapi.com/api/location/1")
             .withCreated("2017-11-10T12:42:04.162Z")
 
-        /**
-         * Creates Citadel of Ricks - The secret headquarters for the Council of Ricks.
-         */
         fun citadelOfRicks() = LocationBuilder()
             .withId(3)
             .withName("Citadel of Ricks")
@@ -85,9 +66,6 @@ class LocationBuilder {
             .withUrl("https://rickandmortyapi.com/api/location/3")
             .withCreated("2017-11-10T13:08:13.191Z")
 
-        /**
-         * Creates Planet Squanch - Squanchy's home planet.
-         */
         fun planetSquanch() = LocationBuilder()
             .withId(35)
             .withName("Planet Squanch")
@@ -97,9 +75,6 @@ class LocationBuilder {
             .withUrl("https://rickandmortyapi.com/api/location/35")
             .withCreated("2017-11-18T11:40:26.310Z")
 
-        /**
-         * Creates an unknown location with minimal data.
-         */
         fun unknownLocation() = LocationBuilder()
             .withId(99)
             .withName("unknown")
